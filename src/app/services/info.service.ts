4d52694090b030c3b00ef45ec683fd0465bd8015
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-05 15:41:02 +0200

    Refactor: API key moved to environment file

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -5,6 +5,7 @@ import { catchError, map, retry, take } from 'rxjs/operators';
 import { ApiResponse, ApiData } from '../models/info.model';
 import { Observable, throwError } from 'rxjs';
 
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
@@ -14,7 +15,7 @@ export class InfoService {
   constructor(public http: HttpClient) {}
 
   public getInfo(offset: string, limit: string): Observable<any> {
-    const apiKey = '';
+    const apiKey = environment.API_KEY;
     const baseUrl = `http://api.mediastack.com/v1/news?access_key=${apiKey}`;
     const options = 'sources=cnn,bbc&languages=en';
     const apiUrl = `${baseUrl}&${options}&offset=${offset}&limit=${limit}`;
@@ -24,7 +25,7 @@ export class InfoService {
       map(res => res.data),
       catchError((err) => {
         return throwError(
-          'There was a problem fetching data from the RATP API, error: ',
+          'There was a problem fetching data from the API, error: ',
           err
         );
       })
