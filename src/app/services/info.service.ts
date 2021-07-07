06a89df00a3673d9e0a31a1c2c8b849e5816d8da
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-07 18:02:51 +0200

    refactor: change API optns & remove unused imports

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, retry, take } from 'rxjs/operators';
+import { catchError, map, take } from 'rxjs/operators';
 
 import { ApiResponse, ApiData } from '../models/info.model';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 @Injectable({
@@ -14,23 +14,21 @@ export class InfoService {
 
   constructor(public http: HttpClient) {}
 
+  // offset set to 0, limit set to 100
   public getInfo(offset: string, limit: string): Observable<any> {
     const apiKey = environment.API_KEY;
     const baseUrl = `http://api.mediastack.com/v1/news?access_key=${apiKey}`;
-    const options = 'sources=cnn,bbc&languages=en';
-    const apiUrl = `${baseUrl}&${options}&offset=${offset}&limit=${limit}`;
+    const languageOption = 'fr';
+    const sortOption = 'published_desc';
+    const apiUrl = `${baseUrl}&language=${languageOption}&sort=${sortOption}&${offset}&${limit}`;
 
     this.apiResponseData = this.http.get<ApiResponse>(apiUrl).pipe(
       take(1),
-      map(res => res.data),
+      map((res) => res.data),
       catchError((err) => {
-        return throwError(
-          'There was a problem fetching data from the API, error: ',
-          err
-        );
+        throw 'an error occured: ' + err;
       })
     );
-    console.log('this.apiResponseData', this.apiResponseData);
     return this.apiResponseData;
   }
 }
